4161b64b5792c42eeddf2b29b13975ffeeb422b8
Darrel Ong <darrelong@Darrels-MacBook-Air.local>
2025-06-23 23:19:07 +0800

    added file_content processing

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -26,3 +26,25 @@ def get_files_info(working_directory, directory=None):
         return "\n".join(list_of_strings)
     except Exception as e:
         return f'Error: {str(e)}'
+
+def get_file_content(working_directory, file_path):
+
+    complete_file_path = os.path.join(working_directory, file_path)
+    abs_complete_file_path = os.path.abspath(complete_file_path)
+    abs_working_dir = os.path.abspath(working_directory)
+
+    if not abs_complete_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.isfile(abs_complete_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    try:
+        with open(abs_complete_file_path, 'r') as file:
+            file_content_string = file.read(10000)
+            if len(file.read(1)) == 0:
+                return file_content_string
+            return f'{file_content_string} \n ...File "{file_path}" truncated at 10000 characters'
+    except Exception as e:
+        return f'Error: {str(e)}'
+
