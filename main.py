9fddd3ccb47d3213207d8a236c8626be10db8a88
Dabrel25 <darrelong25@yahoo.com.ph>
2025-06-29 21:52:05 +0800

    agent capabilities increased

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions import file_commands
 
 
 load_dotenv()
@@ -48,8 +49,8 @@ schema_get_file_content = types.FunctionDeclaration(
         }
     )
 )
-schema_run_python = types.FunctionDeclaration(
-    name="run_python",
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
     description="Runs a Python file and returns its output, constrained to the working directory.",
     parameters=types.Schema(
         type=types.Type.OBJECT,
@@ -80,9 +81,55 @@ schema_write_file = types.FunctionDeclaration(
 )
 available_functions = types.Tool(
     function_declarations=[
-        schema_get_files_info,schema_get_file_content,schema_run_python,schema_write_file,
+        schema_get_files_info,schema_get_file_content,schema_run_python_file,schema_write_file,
     ]
 )
+
+
+function_map = {
+    "get_files_info": file_commands.get_files_info,
+    "get_file_content": file_commands.get_file_content,
+    "run_python_file": file_commands.run_python_file,
+    "write_file":  file_commands.write_file,
+
+}
+
+def call_function(function_call_part, verbose=False):
+
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    function_name = function_call_part.name
+    function_args = function_call_part.args
+
+    if function_name in function_map:
+        function_args["working_directory"] = "./calculator"
+        actual_function = function_map[function_name]
+        function_execution = actual_function(**function_args)
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"result": function_execution},
+                )
+            ],
+        )
+
+    else:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+
+
 system_prompt = """
 You are a helpful AI coding agent.
 
@@ -96,11 +143,25 @@ When a user asks a question or makes a request, make a function call plan. You c
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', 
-    contents = messages,
-    config = types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
-)
+
+for i in range(20):
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents = messages,
+        config = types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
+    )
+    for candidate in response.candidates:
+        messages.append(candidate.content)
+
+    if response.candidates[0].content.parts[0].function_call:
+        for part in response.candidates[0].content.parts:
+            if part.function_call:
+                function_call_result = call_function(part, "--verbose" in sys.argv)
+                messages.append(function_call_result)
+    else:
+        # There are NO function calls - agent is done!
+        print(f"Final response:\n{response.text}")
+        break  # Exit the loop
 
 if ("--verbose" in sys.argv):
     print(f"User prompt: {prompt}")
@@ -109,6 +170,14 @@ if ("--verbose" in sys.argv):
 
 if response.function_calls:
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
+        function_call_result = call_function(function_call_part, "--verbose" in sys.argv)
+
+        # Check if the result has the expected structure and print if verbose
+        if function_call_result.parts[0].function_response.response:
+            if "--verbose" in sys.argv:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+        else:
+            raise Exception("Function call failed to return expected response structure")
 else:
     print(f"Response: {response.text}")
